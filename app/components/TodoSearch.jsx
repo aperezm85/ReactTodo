1f8e08dbab0b4317d20bee1912fcda42ea664484
aperezm85 <aperezm85@gmail.com>
2017-06-25 19:56:33 +0100

    Style Todo Application

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -11,7 +11,7 @@ var TodoSearch = React.createClass({
 
     render: function () {
         return (
-            <div>
+            <div className="container__header">
                 <div className="">
                     <input type="search" className="" ref="searchText" placeholder="Search todos" onChange={this.handleSearch}/>
                 </div>
